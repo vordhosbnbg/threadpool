91304dc7b214c39b07e5a4ac803014e023417857
Boris Lazarov <vordhosbn.bg@gmail.com>
2024-03-25 15:51:56 +0200

    Minor improvements

--- threadpool.h
+++ threadpool.h
@@ -1,66 +1,46 @@
 #pragma once
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <queue>
 #include <thread>
 #include <vector>
-#include <future>
-#include <functional>
-#include <condition_variable>
-#include <queue>
-#include <utility>
-#include <atomic>
-#include <chrono>
 
-class ThreadPool
-{
+class ThreadPool {
 public:
-    ThreadPool(size_t numThreads = std::thread::hardware_concurrency()) : nbThreads(numThreads)
-    {
-    }
+    ThreadPool(size_t numThreads = std::thread::hardware_concurrency()) : nbThreads(numThreads) {}
 
-    ~ThreadPool()
-    {
+    ~ThreadPool() {
         waitUntilIdle();
     }
 
-
-    void start()
-    {
-        for(size_t i=0; i < nbThreads; ++i)
-        {
+    void start() {
+        for(size_t i = 0; i < nbThreads; ++i) {
             threads.emplace_back(&ThreadPool::worker, this);
         }
     }
 
-    void waitUntilIdle()
-    {
+    void waitUntilIdle() {
         acceptsWork = false;
-        for(std::thread& thread : threads)
-        {
+        for(std::thread& thread : threads) {
             thread.join();
         }
         threads.clear();
         acceptsWork = true;
     }
 
-    void worker()
-    {
-        while(true)
-        {
+    void worker() {
+        while(true) {
             std::function<void()> operation;
             {
                 using namespace std::chrono_literals;
 
                 std::unique_lock<std::mutex> lock(mx);
-                cv.wait_for(lock, 10ms, [this]()
-                {
-                    return !queue.empty() || !acceptsWork;
-                });
-                if(!queue.empty())
-                {
+                cv.wait_for(lock, 10ms, [this]() { return !queue.empty() || !acceptsWork; });
+                if(!queue.empty()) {
                     operation = queue.front();
                     queue.pop();
-                }
-                else if(!acceptsWork)
-                {
+                } else if(!acceptsWork) {
                     return;
                 }
             }
@@ -69,10 +49,8 @@ public:
         }
     }
 
-    void push(std::function<void()>&& f)
-    {
-        if(acceptsWork)
-        {
+    void push(std::function<void()>&& f) {
+        if(acceptsWork) {
             {
                 std::unique_lock<std::mutex> lock(mx);
                 queue.push(f);
@@ -80,7 +58,6 @@ public:
             cv.notify_one();
         }
     }
-
 
 private:
     std::vector<std::thread> threads;
